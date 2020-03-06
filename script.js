66ad3e34d7e5146d6d6dfcbceee3ed3f41eaf2d4
Dan Tucker-Saunders <danutuckersaunders@email.com>
2020-03-06 14:19:24 +1100

    Added input elements and tested ranges

--- script.js
+++ script.js
@@ -1,9 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Lets get the variables and constant setup
+const range = {
+  lowercase: [97, 111],
+  uppercase: [65, 90],
+  numerical: [48, 57],
+  special: [33, 47]
+};
+
+function runRange(key, value) {
+  console.log(key);
+}
+
+
 function generatePassword() {
+  /*
+  var fruits, text;
+  fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+  text = "<ul>";
+  fruits.forEach(myFunction);
+  text += "</ul>";
+
+  function myFunction(value) {
+    console.log("<li>" + value + "</li>");
+  }
+
+*/
+ // range.forEach(runRange);
+
+  /*
+  for(i=0;i<range.length;i++) {
+    range[i]
+    for(x=0;x<range[i].length;x++) {
+      console.log(range[x]);
+    }
+  }
+  */
+
+ //fromCharCode();
+
+  for(var x in range) {
+    //console.log(range[x]);
+    
+    var startOfRange = range[x][0],
+        endOfRange = range[x][1],
+        lengthOfRange = endOfRange - startOfRange;
+
+    for(i=0;i<=lengthOfRange;i++){
+      var charCode = startOfRange + i;
+      console.log(String.fromCharCode(charCode));
+    }
+    
+  }
+  
   return 'test';
 }
+
+
+
 
 // Write password to the #password input
 function writePassword() {
